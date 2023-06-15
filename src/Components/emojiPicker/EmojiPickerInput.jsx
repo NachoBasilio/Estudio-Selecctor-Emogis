c44ff884ac4008476095aaf67014328927ea0f9e
IgnaDev <arcortoon@gmail.com>
2023-06-15 14:26:13 -0300

    utilizando useref

---
+++ src/Components/emojiPicker/EmojiPickerInput.jsx
@@ -0,0 +1,16 @@
+import { useRef } from "react"
+import EmojiPicker from "./EmojiPicker"
+
+
+
+export default function EmojiPickerinput() {
+  const ref = useRef(null) 
+
+  return (
+    <>
+      <input ref={ref} type="text" placeholder="Wenas" />
+      <EmojiPicker></EmojiPicker>
+    </>
+
+  )
+}
---
+++ src/Components/emojiPicker/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        symbol: '😀',
+        name: 'Cara Feliz',
+        keywords: 'feliz cara smile happy face'
+    },
+    {
+        symbol: '😃',
+        name: 'Cara Feliz con Ojos Sonrientes',
+        keywords: 'feliz cara smile happy face eyes'
+    },
+    {
+        symbol: '😄',
+        name: 'Cara Feliz con Ojos Sonrientes y Boca Abierta',
+        keywords: 'feliz cara smile happy face eyes open'
+    },
+    {
+        symbol: '🔥',
+        name: 'Fuego',
+        keywords: 'fuego fire' 
+    },
+    {
+        symbol: '👍',
+        name: 'Pulgar Arriba',
+        keywords: 'pulgar arriba thumb up'
+    },
+    {
+        name: 'Corazon',
+        symbol: '❤️',
+        keywords: 'corazon heart love'
+    }
+]
